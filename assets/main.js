c790a0ca4443193d857c776028c2c54c8d4512d3
Karina Castillo <kacastillo.ch@gmail.com>
2026-01-27 00:54:37 +0000

    creacion de arreglo y funciones de calculo con parametros y retorno para funciones

--- assets/main.js
+++ assets/main.js
@@ -2,16 +2,27 @@
 //-Escribir un script simple que muestre mensajes en la consola utilizando console.log()
 //-Usar prompt() para recibir datos del usuario.
 //-Mostrar mensajes personalizados mediante el uso de alert()
-//-Definir variables utilizando las palabras clave let y const
 //-Pedir al usuario que ingrese dos números y almacenarlos en variables.
 //-Implementar las operaciones básicas de suma, resta, multiplicación y división.
 //-Usar estructuras condicionales como if, else y switch para evaluar diferentes situaciones.
-//-Crear un arreglo que contenga una lista de elementos.
 //-Usar los bucles for y while para recorrer los arreglos creados.
 //-Implementar una función específica que permita filtrar elementos del arreglo según una condición determinada
-//-Crear funciones individuales para cada una de las operaciones matemáticas.
-//-Implementar una función que reciba parámetros y retorne un resultado.
+
 //-Llamar funciones dentro de otras funciones para optimizar la estructura del código.
 //-Crear un objeto que contenga propiedades y valores definidos.
 //-Implementar métodos (funciones internas) dentro de un objeto.
-//-Usar un arreglo de objetos y recorrerlo utilizando los métodos map() o forEach()
+//-Usar un arreglo de objetos y recorrerlo utilizando los métodos map() o forEach()}
+
+//-Crear un arreglo que contenga una lista de elementos.
+//-Definir variables utilizando las palabras clave let y const
+const historialCalculo= [];
+
+//-Crear funciones individuales para cada una de las operaciones matemáticas.
+//-Implementar una función que reciba parámetros y retorne un resultado.
+function suma(a, b){return a + b;}
+function resta(a, b){return a - b;}
+function multiplicacion(a, b){return a * b;}
+function division(a, b){
+    if (b === 0) return "Error 👎: No se puede dividir por cero";
+    return a / b;
+}
