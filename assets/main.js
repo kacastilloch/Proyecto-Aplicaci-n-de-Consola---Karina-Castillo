dbeaee9eb5454e4985bd6b6025642ae2c3a52a1f
Karina Castillo <kacastillo.ch@gmail.com>
2026-01-27 03:37:42 +0000

    Conectando menú con switch y funciones de cálculo listas

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,4 @@
-//Requisitos de evaluación
-//-Pedir al usuario que ingrese dos números y almacenarlos en variables.
-//-Implementar las operaciones básicas de suma, resta, multiplicación y división.
-//-Usar estructuras condicionales como if, else y switch para evaluar diferentes situaciones.
-//-Llamar funciones dentro de otras funciones para optimizar la estructura del código.
-
+//Requisitos de evaluación :D
 
 //-Crear un arreglo que contenga una lista de elementos.
 //-Definir variables utilizando las palabras clave let y const
@@ -76,4 +71,69 @@ function mostralHistorial(){
     console.group("Historial de Cálculos");//este console.group es para organizar mejor la salida en consola
     console.table(historialCalculo);//muestra el historial en formato tabla en la consola
     console.groupEnd();//cierra el grupo de consola
+}
+
+function iniciarCalculadora() {
+    console.log("Bienvenido a la Calculadora!");
+
+    let continuar = true;
+    while (continuar) {
+        let opcion = prompt("Seleccione una operación:\n1. Suma ➕\n2. Resta➖\n3. Multiplicación✖️\n4. División➗\n5. Ver Historial\n6. Buscar Operación🕵🏼\n7. Salir");
+        //-Usar estructuras condicionales como if, else y switch para evaluar diferentes situaciones.
+        switch (opcion) {
+            case '1': case '2': case '3': case '4':
+                realizarOperacion(opcion);
+                break
+            case '5':
+                mostralHistorial();
+                break;
+            case '6':
+                buscarOperacion();
+                break;
+            case '7':
+                continuar = false;
+                alert("Gracias por usar la calculadora. ¡Hasta luego!");
+                console.log("Calculadora finalizada.");
+                break;
+            default:
+                alert("Opción no válida. Por favor, seleccione una opción del 1 al 7.");
+                break;    
+            }
+        }
+    }
+
+function realizarOperacion(opcion) {
+    //-Pedir al usuario que ingrese dos números y almacenarlos en variables.
+    let num1 = parseFloat(prompt("Ingrese el primer número:"));
+    let num2 = parseFloat(prompt("Ingrese el segundo número:"));
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Por favor, ingrese números válidos.");
+        return;
+    }
+
+    let resultado;
+    let tipoOperacion = "";
+    let simbolo = "";
+    //-Implementar las operaciones básicas de suma, resta, multiplicación y división.
+    //-Llamar funciones dentro de otras funciones para optimizar la estructura del código.
+    if (opcion === '1') {
+        resultado = suma(num1, num2);
+        tipoOperacion = "suma";
+        simbolo = "+";
+    }else if (opcion === '2') {
+        resultado = resta(num1, num2);
+        tipoOperacion = "resta";
+        simbolo = "-";
+    }else if (opcion === '3') {
+        resultado = multiplicacion(num1, num2);
+        tipoOperacion = "multiplicacion";
+        simbolo = "x";
+    }else if (opcion === '4') {
+        resultado = division(num1, num2);
+        tipoOperacion = "division";
+        simbolo = "/";
+    }
+    alert(`El resultado de la ${tipoOperacion} es: ${resultado}`);
+    guardarCalculo(tipoOperacion, num1, num2, simbolo, resultado);
 }
