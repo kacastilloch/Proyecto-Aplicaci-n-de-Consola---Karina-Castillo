ec23d85be2055f337d9f6d28b29b0c73392d18a7
Karina Castillo <kacastillo.ch@gmail.com>
2026-01-27 00:29:13 +0000

    se añadieron losrequisitos de evaluación

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+//Requisitos de evaluación
+//-Escribir un script simple que muestre mensajes en la consola utilizando console.log()
+//-Usar prompt() para recibir datos del usuario.
+//-Mostrar mensajes personalizados mediante el uso de alert()
+//-Definir variables utilizando las palabras clave let y const
+//-Pedir al usuario que ingrese dos números y almacenarlos en variables.
+//-Implementar las operaciones básicas de suma, resta, multiplicación y división.
+//-Usar estructuras condicionales como if, else y switch para evaluar diferentes situaciones.
+//-Crear un arreglo que contenga una lista de elementos.
+//-Usar los bucles for y while para recorrer los arreglos creados.
+//-Implementar una función específica que permita filtrar elementos del arreglo según una condición determinada
+//-Crear funciones individuales para cada una de las operaciones matemáticas.
+//-Implementar una función que reciba parámetros y retorne un resultado.
+//-Llamar funciones dentro de otras funciones para optimizar la estructura del código.
+//-Crear un objeto que contenga propiedades y valores definidos.
+//-Implementar métodos (funciones internas) dentro de un objeto.
+//-Usar un arreglo de objetos y recorrerlo utilizando los métodos map() o forEach()
